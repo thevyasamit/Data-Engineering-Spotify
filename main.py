14784184a18cb7cb3fcf2bcda70ae6a75be1e427
Amit Vyas <23amitvyas@gmail.com>
2022-06-13 20:14:00 -0400

    Adding code/ function for validation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from lib2to3.pgen2 import token
 from tokenize import Token
 from wsgiref import headers
 from matplotlib import artist
+from matplotlib.ft2font import BOLD
 import sqlalchemy
 import  requests
 import json
@@ -12,7 +13,18 @@ import pandas as pd
 
 DATABASE_LOCATION = "sqlite:///my_tracks.sqlite"
 USER_ID = "YOUR_SPOTIFY_USER_NAME"
-TOKEN = "GET_TOKEN_FROM_SPOTIFY_DEVELOPER_ACCOUNT"
+TOKEN = "GET_YOUR_TOKEN"
+def check_valid_data(df: pd.DataFrame) -> bool:
+
+    if df.empty:
+        print("No Data")
+        return False
+    
+    if df.isnull().values.any():
+        raise Exception("Null Value found.")
+
+# This is extraction of the data.
+
 if __name__ == "__main__":
     headers = {
         "Accept" : "application/json",
@@ -23,7 +35,7 @@ if __name__ == "__main__":
     
     r = requests.get("https://api.spotify.com/v1/me/player/recently-played", headers = headers)
     data = r.json()
-    
+
     song_names = []
     artist_names = []
     
@@ -37,4 +49,9 @@ if __name__ == "__main__":
     }
     
     songs_df = pd.DataFrame(songs_di, columns=["song_name","artist_names"])
-    print(songs_df)
+    
+    
+    # This is Transforming/checking/Validating data 
+    
+    if check_valid_data(songs_df):
+        print("Data is Valid")
